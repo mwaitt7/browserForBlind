c0f9551e2835551a9e1a56e72a303fba70fd5ddf
Michael <waitt18@up.edu>
2017-01-31 13:51:38 -0800

    class and main added

---
+++ src/browser.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+/**
+ * Browser - The main class of the browser, contains main and all necessary methods.
+ *
+ * @author Michael Waitt
+ * @version 1.0
+ */
+public class browser {
+
+
+
+    public static void main(String args[]) {
+
+    }
+}
